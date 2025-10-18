73e81daf28a2f25ae38789a6aa11fe61586115e2
lcier835 <104859919+lcier835@users.noreply.github.com>
2025-10-18 12:33:02 -0700

    Add Pedestal Buttons

---
+++ testmechanics/pedestalbutton.gd
@@ -0,0 +1,24 @@
+class_name PedestalButton extends StaticBody2D
+
+var pressTimer = 1
+@export var pressTime = 0.5
+
+signal OnButtonPressed
+signal OnButtonUnpressed
+
+func _interacted_by_player(_player: Player):
+	if pressTimer > pressTime || (pressTime == -1 && pressTimer != -1):
+		$Sprite2D.modulate = Color(1, 1, 0)
+		emit_signal("OnButtonPressed")
+		if pressTime == -1:
+			pressTimer = -1
+		else:
+			pressTimer = 0
+
+func _process(_delta: float) -> void:
+	if pressTimer < pressTime && (pressTimer + _delta) >= pressTime:
+		$Sprite2D.modulate = Color(1, 1, 1)
+		emit_signal("OnButtonUnpressed")
+	
+	if pressTime != -1:
+		pressTimer += _delta
