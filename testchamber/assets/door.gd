e16a0a1a44da79c035cfad57484eb5c191a78181
lcier835 <104859919+lcier835@users.noreply.github.com>
2025-10-18 23:53:54 -0700

    Add test chamber doors

---
+++ testchamber/assets/door.gd
@@ -0,0 +1,40 @@
+class_name Door extends StaticBody2D
+
+@export var leftSideBlack: bool = false
+@export var rightSideBlack: bool = false
+@export var startOpen: bool = false
+var open = false
+var wantsToClose = false
+
+func _ready() -> void:
+	open = startOpen
+
+func updateSprite(isTop: bool, isLeft: bool, sprite: Sprite2D):
+	var index = 0
+	var black = (isLeft && leftSideBlack) || (!isLeft && rightSideBlack)
+	if !black: index += 4
+	if !isTop: index += 2
+	if open: index += 1
+	sprite.frame = index
+
+func updateSprites():
+	updateSprite(false, false, $RdoorBottom)
+	updateSprite(true, false, $RdoorTop)
+	updateSprite(false, true, $LdoorBottom)
+	updateSprite(true, true, $LdoorTop)
+
+func openDoor():
+	open = true
+	$BlockerMiddle.disabled = true
+	updateSprites()
+
+func closeDoor():
+	wantsToClose = true
+
+func _process(delta: float) -> void:
+	print($PlayerBlocker.has_overlapping_bodies())
+	if wantsToClose && !$PlayerBlocker.has_overlapping_bodies():
+		open = false
+		updateSprites()
+		$BlockerMiddle.disabled = false
+		wantsToClose = false
